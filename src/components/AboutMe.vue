beef0b18404bc5be612f5bdd569ecf6f9c05726b
Ari <arisenger0@gmail.com>
2024-08-13 08:19:21 +0200

    fix: about

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,12 +1,14 @@
 <template>
-  <section id="about" class="h-screen">
+  <section>
     <div>
-      <h1 class="text-8xl" style="text-shadow: 2px 2px 8px #99ff99">Ari</h1>
-      <h2 class="text-5xl" style="text-shadow: 2px 2px 8px #99ff99">Junior Frontend Developer</h2>
+      <h1 class="text-8xl text-black" style="text-shadow: 2px 2px 8px #99ff99">Ari</h1>
+      <h2 class="text-5xl text-black" style="text-shadow: 2px 2px 8px #99ff99">
+        Junior Frontend Developer
+      </h2>
     </div>
 
     <div class="flex gap-12 py-10 mx-auto">
-      <p class="text-2xl leading-10">
+      <p class="text-2xl leading-10 text-black">
         I have a background in philosophy and a passion for coding. Since 2021, I’ve built a strong
         technical foundation through self-directed learning and recently completed a frontend
         development program with Coding Bootcamps Europe. I’m excited about solving problems,
@@ -17,24 +19,26 @@
       </div>
     </div>
 
-    <div class="mb-4">
-      <h4 class="text-5xl" style="text-shadow: 2px 2px 8px #99ff99">skills & tools</h4>
-      <div class="my-2">
-        <p class="text-2xl">I learned and have extended experience with:</p>
-        <div class="py-4">
-          <p class="bg-green-100 p-1 m-1 inline text-xl">Vue.js</p>
-          <p class="bg-green-100 p-1 m-1 inline text-xl">JavaScript</p>
-          <p class="bg-green-100 p-1 m-1 inline text-xl">HTML</p>
-          <p class="bg-green-100 p-1 m-1 inline text-xl">CSS</p>
-          <p class="bg-green-100 p-1 m-1 inline text-xl">Git & GitHub</p>
+    <div class="mt-10">
+      <h4 class="text-5xl text-black" style="text-shadow: 2px 2px 8px #99ff99">skills & tools</h4>
+      <div class="flex justify-around my-2">
+        <div class="">
+          <p class="text-2xl text-black">I have extended experience with:</p>
+          <div class="py-4 text-black">
+            <p class="bg-green-100 p-1 m-1 inline text-xl">Vue.js</p>
+            <p class="bg-green-100 p-1 m-1 inline text-xl">JavaScript</p>
+            <p class="bg-green-100 p-1 m-1 inline text-xl">HTML</p>
+            <p class="bg-green-100 p-1 m-1 inline text-xl">CSS</p>
+            <p class="bg-green-100 p-1 m-1 inline text-xl">Git & GitHub</p>
+          </div>
         </div>
-      </div>
-      <div>
-        <p class="text-2xl">I have basic experience with:</p>
-        <div class="py-4">
-          <p class="bg-green-100 p-1 m-1 inline text-xl">Postman</p>
-          <p class="bg-green-100 p-1 m-1 inline text-xl">TypeScript</p>
-          <p class="bg-green-100 p-1 m-1 inline text-xl">Node.js</p>
+        <div>
+          <p class="text-2xl text-black">I have basic experience with:</p>
+          <div class="py-4 text-black">
+            <p class="bg-green-100 p-1 m-1 inline text-xl">TypeScript</p>
+            <p class="bg-green-100 p-1 m-1 inline text-xl">Node.js</p>
+            <p class="bg-green-100 p-1 m-1 inline text-xl">Postman</p>
+          </div>
         </div>
       </div>
     </div>
